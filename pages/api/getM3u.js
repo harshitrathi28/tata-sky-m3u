744758ad1903abf186fba5e07e666fbddb8f1efb
harshitrathi28 <155360887+harshitrathi28@users.noreply.github.com>
2024-06-15 15:04:32 +0530

    Update getM3u.js

--- pages/api/getM3u.js
+++ pages/api/getM3u.js
@@ -140,9 +140,9 @@ const getUserChanDetails = async (userChannels) => {
 
     while (chanIds.length > 0) {
         chanIdsStr = chanIds.splice(0, 99).join(',');
-        await fetch("https://tm.tapi.videoready.tv/content-detail/pub/api/v1/live-tv-genre/channels?genre=&language=&channelIds=" + chanIdsStr, requestOptions)
+        await fetch("https://ts-more-api.videoready.tv/content-detail/pub/api/v2/channels/" + chanIdsStr, requestOptions)
             .then(response => response.json())
-            .then(cData => result.push(...cData.data.liveChannels))
+            .then(cData => result.push(cData.data))
             .catch(error => {
                 console.log('error: ', error);
                 err = error;
